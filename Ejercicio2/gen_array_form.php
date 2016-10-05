50eb1293d8f15a67a65abd93d00656553c138211
Yeray <ylagef@gmail.com>
2016-10-05 20:36:04 +0200

    Ej2 en proceso.

---
+++ Ejercicio2/gen_array_form.php
@@ -0,0 +1,24 @@
+<?php
+
+$servername = "localhost";
+$username = "iu2016";
+$password = "iu2016";
+$dbname = "IU2016";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "show columns from USUARIOS";
+$result = $conn->query($sql);
+
+$result->fetch_assoc();
+
+var_dump($result);
+
+$conn->close();
+
+?>
